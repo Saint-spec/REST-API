3cdb7f0f56b0085cd0fc528bc124e87bed52352c
Saint-spec <christiancervantes02@gmail.com>
2023-07-08 21:10:35 -0400

    created server, created subscriber router, created route

---
+++ routes/subscribers.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const router = express.Router()
+
+// get all
+router.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+//getting one
+router.get('/:id', (req, res) => {
+
+})
+
+//creating one
+router.post('/', (req, res) => {
+
+})
+
+//updating one
+router.patch('/:id', (req, res) => {
+
+})
+
+//delete one
+router.delete('/:id', (req, res) => {
+
+})
+
+module.exports = router
