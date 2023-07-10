89841f80f5588c0a200b7934a6a0e29df3c1b943
Saint-spec <christiancervantes02@gmail.com>
2023-07-10 14:39:16 -0400

    subscribers is not defined

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Subscriber = require("../models/subscriber");
+const subscriber = require("../models/subscriber");
 
 //get all
 router.get("/", async (req, res) => {
@@ -13,17 +14,42 @@ router.get("/", async (req, res) => {
 });
 
 //get one
-router.get("/:id", (req, res) => {
-  res.send(req.params.id);
+router.get("/:id", getSubscriber, (req, res) => {
+  res.send(res.subscriber.name);
 });
 
 //creating one
-router.post("/", async (req, res) => {});
+router.post("/", async (req, res) => {
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscribedToChannel: req.body.subscribedToChannel,
+  });
+  try {
+    const newSubscriber = await subscriber.save();
+    res.status(201).json(newSubscriber);
+  } catch (err) {
+    res.status(400).json({ meassge: err.message });
+  }
+});
 
 //updating one
 router.patch("/:id", (req, res) => {});
 
 //deleting one
 router.patch("/:id", (req, res) => {});
+
+async function getSubscriber(req, res, next) {
+  let subscriber;
+  try {
+    subscriber = await Subscriber.findById(req.params.id);
+    if (subscriber == null) {
+      return res.status(404).json({ message: "Cannot find subscriber" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+  res.subscriber = subscriber;
+  next();
+}
 
 module.exports = router;
